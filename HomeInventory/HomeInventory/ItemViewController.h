be42e1b3c0d263dbaefbbda8ac647fb4e1fb95b3
New <eric.braen@gmail.com>
2014-08-21 15:54:06 -0700

    starting chap 8

---
+++ HomeInventory/HomeInventory/ItemViewController.h
@@ -0,0 +1,13 @@
+//
+//  ItemViewController.h
+//  HomeInventory
+//
+//  Created by New on 8/21/14.
+//  Copyright (c) 2014 New. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ItemViewController : UITableViewController
+
+@end
---
+++ HomeInventory/HomeInventory/ItemViewController.m
@@ -0,0 +1,24 @@
+//
+//  ItemViewController.m
+//  HomeInventory
+//
+//  Created by New on 8/21/14.
+//  Copyright (c) 2014 New. All rights reserved.
+//
+
+#import "ItemViewController.h"
+
+@implementation ItemViewController
+
+- (instancetype)init
+{
+    self = [super initWithStyle:UITableViewStylePlain];
+    return self;
+}
+
+- (instancetype)initWithStyle:(UITableViewStyle)style
+{
+    return [self init];
+}
+
+@end
